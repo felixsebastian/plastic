01b6b4926c64a72e87423575ea05dd784b5ff689
Felix Crotty <felix.crotty@cultureamp.com>
2022-09-19 08:58:11 +1000

    update divider height

--- src/Divider.tsx
+++ src/Divider.tsx
@@ -25,7 +25,7 @@ const Divider = (props: Props) => {
       css={css`
         margin: 0;
         border-width: 0;
-        border-top: ${props.type === "menu" ? s.xxxs : s.xs} solid;
+        border-top: ${props.type === "menu" ? s.xxxs : s.xxs} solid;
         border-color: ${c.gray200};
         border-color: ${theme.color(color).rgba(opacity)};
       `}
