26680ca0767ccaba1770c4ad0bf3122c3ec5d519
Felix Crotty <felix.crotty@cultureamp.com>
2022-09-20 17:33:16 +1000

    row refinements

--- src/Row.tsx
+++ src/Row.tsx
@@ -3,16 +3,30 @@ import { Box, useTheme } from "@specimen/foundation";
 import { Children } from "react";
 import Column from "./Column";
 
-const Row = ({ children, gap = "md", simple }: any) => {
+const alignments = {
+  top: "flex-start",
+  bottom: "flex-end",
+  center: "center",
+};
+
+const Row = ({
+  children,
+  gap = "md",
+  simple,
+  alignY = "top",
+  ...props
+}: any) => {
   const { s } = useTheme();
 
   return (
     <Box
       d="flex"
       css={css`
-        flex-wrap: nowrap;
         gap: ${s[gap]};
+        align-items: ${alignments[alignY]};
+        flex-wrap: nowrap;
       `}
+      {...props}
     >
       {simple
         ? Children.map(children, (child) => <Column>{child}</Column>)
